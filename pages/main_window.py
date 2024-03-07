eafa451c68dead5137c18fe0f1fe609da63b5ec6
EfeErkan <e.erkan@ug.bilkent.edu.tr>
2024-03-07 14:54:48 +0300

    Position of Logout fixed

--- pages/main_window.py
+++ pages/main_window.py
@@ -1,12 +1,9 @@
 from PyQt5.QtWidgets import *
-from PyQt5.QtCore import Qt, QDateTime
 from pages.login_page import LoginScreen
 from pages.camera_page import CameraPage
-from pages.employee_page import EmployeePage
 from pages.video_page import VideoPage
 from pages.main_page import MainPage
 import utils
-
 
 class MainWindow(QMainWindow):
    def __init__(self):
@@ -19,13 +16,6 @@ class MainWindow(QMainWindow):
       self.setGeometry(300, 300, 900, 900)
 
       menubar = self.menuBar()
-      file_menu = menubar.addMenu('Logout')
-
-      logout_action = QAction('Logout', self)
-
-      if utils.is_user_logged_out():
-          logout_action.triggered.connect(self.close)
-      file_menu.addAction(logout_action)
 
       self.center_on_screen()
       # Add other menu items and functionalities as needed
@@ -33,13 +23,11 @@ class MainWindow(QMainWindow):
       self.setCentralWidget(self.stacked_widget)
       self.main_page = MainPage()
       self.camera_page = CameraPage()
-      # self.employee_page = EmployeePage()
       self.video_page = VideoPage()
 
       self.stacked_widget.addWidget(self.main_page)
       self.stacked_widget.addWidget(self.camera_page)
       self.stacked_widget.addWidget(self.video_page)
-      # self.stacked_widget.addWidget(self.employee_page)
       self.stacked_widget.setCurrentWidget(self.main_page)
 
       # Create actions to switch between pages
@@ -48,9 +36,6 @@ class MainWindow(QMainWindow):
 
       camera_action = QAction('Camera Page', self)
       camera_action.triggered.connect(lambda: self.switch_page(self.camera_page))
-
-      # employee_action = QAction('Employee Page', self)
-      # employee_action.triggered.connect(lambda: self.switch_page(self.employee_page))
       
       video_action = QAction('Video Page', self)
       video_action.triggered.connect(lambda: self.switch_page(self.video_page))
@@ -58,7 +43,15 @@ class MainWindow(QMainWindow):
       menubar.addAction(main_action)
       menubar.addAction(camera_action)
       menubar.addAction(video_action)
-      # menubar.addAction(employee_action)
+      
+      file_menu = menubar.addMenu('Logout')
+
+      logout_action = QAction('Logout', self)
+
+      if utils.is_user_logged_out():
+          logout_action.triggered.connect(self.close)
+          
+      file_menu.addAction(logout_action)
       
       with open('styles/main_window_styles.qss', 'r') as style_file:
          self.setStyleSheet(style_file.read())
