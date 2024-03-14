d2d03c8eb5ffd6c582830d871cdd0b7184525faa
EfeErkan <e.erkan@ug.bilkent.edu.tr>
2024-03-14 20:01:53 +0300

    Adding Styles to the spinner

--- components/spinner.py
+++ components/spinner.py
@@ -7,19 +7,26 @@ class SpinnerDialog(QDialog):
       super().__init__()
 
       self.setWindowTitle("Spinner Example")
-      self.setGeometry(100, 100, 400, 300)
+      self.setGeometry(100, 100, 400, 200)
 
       layout = QVBoxLayout()
+      layout.setSpacing(20)
+      layout.setContentsMargins(40, 40, 40, 40)
 
       self.progress_bar = QProgressBar()
-      self.progress_bar.setAlignment(Qt.AlignHCenter)
+      self.progress_bar.setAlignment(Qt.AlignCenter)
       self.progress_bar.setRange(0, 0)
-      self.progress_bar.setValue(0)
-      
+
+      self.progress_bar.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
+
       self.label = QLabel(label)
-      self.label.setAlignment(Qt.AlignHCenter)
+      self.label.setAlignment(Qt.AlignCenter)
+      self.label.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
 
       layout.addWidget(self.label)
       layout.addWidget(self.progress_bar)
-      
+
       self.setLayout(layout)
+      
+      with open('styles/spinner_styles.qss', 'r') as style_file:
+         self.setStyleSheet(style_file.read())
