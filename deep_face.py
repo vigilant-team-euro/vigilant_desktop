12e40a82c339c943ab9634fe1c0769cc3881c7be
EfeErkan <e.erkan@ug.bilkent.edu.tr>
2024-03-22 22:08:11 +0300

    Variable names and logic fixed

--- deep_face.py
+++ deep_face.py
@@ -4,49 +4,43 @@ from deepface import DeepFace
 import datetime
 import firebase
 
-def createFrame():
-      list = {
-         "average_age": 0,
-         "customer_count":0,
-         "fear_count":0,
-         "happy_count":0,
-         "neutral_count":0,
-         "sad_count":0,
-         "surprise_count":0,
-         "female_count":0,
-         "male_count":0,
-         "start_date": datetime.datetime.now(),
-         "end_date": datetime.datetime.now(),
-         "frame_id":0,
-      }
-      return list
+FIREBASE_PUSH_INTERVAL_SECONDS = 60
+SECONDS_IN_MINUTE = 60
 
-def pushResult(result:dict, list:list):
-   for result_list in result:
-      emotion = result_list["dominant_emotion"]
-      gender = result_list["dominant_gender"]
-      age = result_list["age"]
-
-      if emotion == "sad":
-         list["sad_count"] += 1
-      if emotion == "fear":
-         list["fear_count"] += 1
-      if emotion == "happy":
-         list["happy_count"] += 1
-      if emotion == "neutral":
-         list["neutral_count"] += 1
-      if emotion == "surprise":
-         list["surprise_count"] += 1
-
+def construct_total_result():
+   return {
+      "total_age": 0,
+      "total_customer_count": 0,
+      "total_fear_count": 0,
+      "total_happy_count": 0,
+      "total_neutral_count": 0,
+      "total_sad_count": 0,
+      "total_surprise_count": 0,
+      "total_female_count": 0,
+      "total_male_count": 0,
+      "start_date": datetime.datetime.now(),
+      "end_date": datetime.datetime.now(),
+      "frame_id":0,
+   }
+   
+def update_total_result(deepface_result, total_result):
+   for person in deepface_result:
+      emotion = person["dominant_emotion"]
+      gender = person["dominant_gender"]
+      age = person["age"]
+      
+      total_result[f"total_{emotion}_count"] += 1
+      
       if gender == "Man":
-         list["male_count"] += 1
-      if gender == "Woman":
-         list["female_count"] += 1
+         total_result["total_male_count"] += 1
+      else:
+         total_result["total_female_count"] += 1
+         
+      total_result["total_age"] += age
+      total_result["total_customer_count"] += 1
+      
 
-      list["average_age"] += age
-      list["customer_count"] += 1
-
-def deep_face(video_path:str, interval_seconds:int, username:str, store_name:str, date: datetime):
+def deep_face(video_path:str, frame_interval_seconds:int, username:str, store_name:str, date: datetime):
    output_folder = "output_folder"
 
    cap = cv2.VideoCapture(video_path)
@@ -55,41 +49,47 @@ def deep_face(video_path:str, interval_seconds:int, username:str, store_name:str
    result_count = 0
    frame_id = 1
    start_date = date
-   end_date = date + datetime.timedelta(minutes=3)
+   end_date = date + datetime.timedelta(minutes=FIREBASE_PUSH_INTERVAL_SECONDS / SECONDS_IN_MINUTE)
 
    frames_arr = []
-   frame = {}
-   frameResult = createFrame()
-   result = []
+   total_result = construct_total_result()
 
    while True:
       ret, frame = cap.read()
       if not ret:
-         frameResult["average_age"] = (int) (frameResult["average_age"] / frameResult["customer_count"])
-         frameResult["frame_id"] = frame_id
-         frameResult["start_date"] = start_date
-         frameResult["end_date"] = end_date
-         frames_arr.append(frameResult)
          break
 
-      if frame_number % (fps * interval_seconds) == 0:
+      if frame_number % (fps * frame_interval_seconds) == 0:
          output_path = os.path.join(output_folder, f"frame_{frame_number}.jpg")
          cv2.imwrite(output_path, frame)
          result = DeepFace.analyze(output_path, enforce_detection=False)
-         pushResult(result, frameResult)
+         update_total_result(result, total_result)
          result_count += 1
          
-         if result_count % (180 / interval_seconds) == 0:
-            frameResult["average_age"] = (int) (frameResult["average_age"] / frameResult["customer_count"])
-            frameResult["frame_id"] = frame_id
-            frameResult["start_date"] = start_date
-            frameResult["end_date"] = end_date
+         if result_count % (FIREBASE_PUSH_INTERVAL_SECONDS / frame_interval_seconds) == 0:
+            
+            num_of_frames = FIREBASE_PUSH_INTERVAL_SECONDS / frame_interval_seconds
 
+            average_result = {
+               "average_age": total_result["total_age"] / total_result["total_customer_count"],
+               "customer_count": total_result["total_customer_count"] / num_of_frames,
+               "fear_count": total_result["total_fear_count"] / num_of_frames,
+               "happy_count": total_result["total_happy_count"] / num_of_frames,
+               "neutral_count": total_result["total_neutral_count"] / num_of_frames,
+               "sad_count": total_result["total_sad_count"] / num_of_frames,
+               "surprise_count": total_result["total_surprise_count"] / num_of_frames,
+               "female_count": total_result["total_female_count"] / num_of_frames,
+               "male_count": total_result["total_male_count"] / num_of_frames,
+               "start_date": start_date,
+               "end_date": end_date,
+               "frame_id": frame_id,
+            }
+            
             frame_id += 1
-            start_date += datetime.timedelta(minutes=3)
-            end_date += datetime.timedelta(minutes=3)
-            frames_arr.append(frameResult)
-            frameResult = createFrame()
+            start_date += datetime.timedelta(minutes=FIREBASE_PUSH_INTERVAL_SECONDS / SECONDS_IN_MINUTE)
+            end_date += datetime.timedelta(minutes=FIREBASE_PUSH_INTERVAL_SECONDS / SECONDS_IN_MINUTE)
+            frames_arr.append(average_result)
+            total_result = construct_total_result()
 
 
       frame_number += 1
@@ -100,8 +100,6 @@ def deep_face(video_path:str, interval_seconds:int, username:str, store_name:str
    # remove files
    files = os.listdir(output_folder)
    for file in files:
-      os.remove(f"{output_folder}/{file}")  
+      os.remove(os.path.join(output_folder, file))
 
    cap.release()
-
-
