9d80c8baf5733c3ca84bc777ae31083dff8a89b0
Recep Uysal <recep.uysal@ug.bilkent.edu.tr>
2024-04-21 13:02:45 +0300

    Update randomDataGenerator.py

--- randomDataGenerator.py
+++ randomDataGenerator.py
@@ -7,10 +7,12 @@ import random
 cred = credentials.Certificate("./firebaseConfig.json")
 app = firebase_admin.initialize_app(cred, {'storageBucket': 'vigilant-36758.appspot.com'})
 all_db = firestore.client()
+AGE_INTERVALS = [0, 15, 30, 45, 60]
 
 def pushRandomData(username:str, storename:str, day:int, month:int, year:int, interval:int):
     
     start_date = datetime.datetime(year, month, day, 0, 0)
+    date = datetime.datetime(year, month, day, 0, 0, 0)
     end_date = start_date + timedelta(minutes=interval)
     frame_id = 1
     
@@ -22,7 +24,6 @@ def pushRandomData(username:str, storename:str, day:int, month:int, year:int, in
         male_count = random.randint(0,55)
 
         arr = {
-        "average_age": random.randint(10,55),
         "customer_count": female_count + male_count,
         "fear_count": random.randint(0,10),
         "female_count": female_count,
@@ -35,6 +36,9 @@ def pushRandomData(username:str, storename:str, day:int, month:int, year:int, in
         "start_date": start_date,
         "end_date": end_date
         }
+
+        for i in range(len(AGE_INTERVALS) - 1):
+            arr[f"{AGE_INTERVALS[i]}-{AGE_INTERVALS[i + 1]}_age_count"] = random.randint(0,10)
         
         frames_arr.append(arr)
 
@@ -44,9 +48,10 @@ def pushRandomData(username:str, storename:str, day:int, month:int, year:int, in
         start_date += timedelta(minutes=interval)
         end_date += timedelta(minutes=interval)
         frame_id += 1
-        
+
     data = {
             "storeName": "store1",
+            "date": date,
             "frames": frames_arr
         }
 
@@ -54,7 +59,12 @@ def pushRandomData(username:str, storename:str, day:int, month:int, year:int, in
 
 
 if __name__ == '__main__':
-    pushRandomData("random_user2", "store1", 8, 3, 2024, 3)
-    pushRandomData("random_user2", "store1", 9, 3, 2024, 5)
-    pushRandomData("random_user2", "store1", 10, 3, 2024, 10)
-    pushRandomData("random_user2", "store1", 11, 3, 2024, 15)
+    pushRandomData("F3sSGpLf1YORxmOPuniEBCUZrH22", "17kG38Y5tTJWh74TyM84", 1, 4, 2024, 3)
+    pushRandomData("F3sSGpLf1YORxmOPuniEBCUZrH22", "17kG38Y5tTJWh74TyM84", 2, 4, 2024, 5)
+    pushRandomData("F3sSGpLf1YORxmOPuniEBCUZrH22", "17kG38Y5tTJWh74TyM84", 3, 4, 2024, 10)
+    pushRandomData("F3sSGpLf1YORxmOPuniEBCUZrH22", "17kG38Y5tTJWh74TyM84", 4, 4, 2024, 15)
+    pushRandomData("F3sSGpLf1YORxmOPuniEBCUZrH22", "17kG38Y5tTJWh74TyM84", 5, 4, 2024, 20)
+    pushRandomData("F3sSGpLf1YORxmOPuniEBCUZrH22", "17kG38Y5tTJWh74TyM84", 6, 4, 2024, 6)
+    pushRandomData("F3sSGpLf1YORxmOPuniEBCUZrH22", "17kG38Y5tTJWh74TyM84", 7, 4, 2024, 12)
+    pushRandomData("F3sSGpLf1YORxmOPuniEBCUZrH22", "17kG38Y5tTJWh74TyM84", 8, 4, 2024, 5)
+    pushRandomData("F3sSGpLf1YORxmOPuniEBCUZrH22", "17kG38Y5tTJWh74TyM84", 9, 4, 2024, 10)
